2a06b4c084f6e4b8739a9bc5b7677f91fc7ccca5
lgbouma <lgbouma@mit.edu>
2017-01-05 17:51:34 -0500

    ran DEBiL. functional, but not too sure. see 170501 reorient.

--- src/UTIL_plot_DEBiL_LCs.py
+++ src/UTIL_plot_DEBiL_LCs.py
@@ -8,15 +8,20 @@ field = 'G199/'
 DSP_lim = 30
 
 head_path = '../data/DEBiL_heads/'+field[:-1]+'_DSP'+str(DSP_lim)+'.txt'
-DEBiL_head = np.genfromtxt(head_path, dtype=['U15',float], names=['hatid', 'blsperiod'])
+DEBiL_head = np.genfromtxt(head_path, dtype=['U15',float], 
+        names=['hatid', 'blsperiod'])
 
 plt.ioff()
 plt.close('all')
 for name, period in DEBiL_head:
     LC_path = '../data/DEBiL_LCs/'+field+name+'.txt'
-    LC = np.genfromtxt(LC_path, dtype=[np.float64,np.float64,np.float64], names=['time','epd000','err'])
+    LC = np.genfromtxt(LC_path, dtype=[np.float64,np.float64,np.float64],
+            names=['time','epd000','err'])
     out_file = '../results/prelim/'+name+'.png'
     if not os.path.exists(out_file):
-        pb.plot_phased_mag_series(LC['time'], LC['epd000'], period, outfile=out_file)
+        pb.plot_phased_mag_series(LC['time'], LC['epd000'], period,
+                outfile=out_file, plotphaselim=[-0.6,0.6],
+                vanderburg_style=True, out_dpi=300)
     else:
         continue
+
