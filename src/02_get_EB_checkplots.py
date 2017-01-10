1ef1ee06997f587ced09a6159bdc18535eac90b7
lgbouma <lgbouma@mit.edu>
2017-01-10 17:30:23 -0500

    see changelog

--- src/02_get_EB_checkplots.py
+++ src/02_get_EB_checkplots.py
@@ -1,9 +1,9 @@
 '''
 Given field, DSP_lim, and LCs, find first-pass at interesting EB candidates by
-periodicity analysis.
+periodicity analysis. Return EB checkplots.
 
 Usage (from within hat/src/):
-> python 03_find_LC_period_given_field.py field_id DSP_lim
+> python 02_get_EB_checkplots.py field_id DSP_lim > foo.out
 Args:
     field_id: integer specifying HAT field ID number (HAT-???-1234567).
 
@@ -13,6 +13,7 @@ Non-understood bugs:
 '''
 
 import subprocess, os, pipes
+import requests, os, time
 import pandas as pd, numpy as np
 from astropy.io import ascii
 from astropy.table import vstack
@@ -52,29 +53,38 @@ def run_script(script, stdin=None):
     stdout, stderr = proc.communicate()
     return stdout, stderr
 
-def get_blsanalsum_output(field_id=None):
-    '''
-    This script copies BLS results (blsanalsum.txt files) from a run of
-    HATpipe, hosted on the HAT servers, locally for processing.
-    '''
-    assert type(field_id) == str, 'call get_blsanalsum_output with field_id'
-
-    # Given neighboring fields, get paths on HATNet servers to download BLS
-    # pipeline output
-    host_name = 'lbouma@phn12.astro.princeton.edu'
-    base_dir='/H/BIGPROJ/hatuser/2007_hatnet_phot/'
+def get_neighboring_fields(field_id):
+    '''Given a field ID, get paths of neighboring fields, return a list of the
+    fields, and a list of their paths on the HAT computers. '''
 
     # 2 lines of `#`-prepended comments, then lines of neighboring fields.
     # Each field is 3 digits, so narrow indexing below works.
     fo = open('DATA_nbhr_fields.txt', 'r')
     nbhr_lines = fo.readlines()
     fo.close()
-    nbhr_fields_line = [l for l in nbhr_lines[2:] \
+    nbhr_fields_line = [l for l in nbhr_lines[2:]
             if int(l[:3]) == int(field_id)]
     assert len(nbhr_fields_line) == 1, 'nbhr_fields_line should be singleton'
     nbhr_fields = nbhr_fields_line[0][4:-1].split(',')
     nbhr_field_subdirs = list(map(lambda x:'G'+x+'/', nbhr_fields))
+
+    base_dir='/H/BIGPROJ/hatuser/2007_hatnet_phot/'
     nbhr_field_paths = [base_dir+sd for sd in nbhr_field_subdirs]
+
+    return nbhr_fields, nbhr_field_paths
+
+def scp_blsanalsum_output(field_id=None):
+    '''
+    This script copies BLS results (blsanalsum.txt files) from a run of
+    HATpipe, hosted on the HAT servers, locally for processing.
+    '''
+    assert type(field_id) == str, 'call scp_blsanalsum_output with field_id'
+
+    # Given neighboring fields, get paths on HATNet servers to download BLS
+    # pipeline output
+    host_name = 'lbouma@phn12.astro.princeton.edu'
+
+    nbhr_fields, nbhr_field_paths = get_neighboring_fields(field_id)
     write_path = '../data/HATpipe/blsanalsums/'
 
     for nbhr_field_path in nbhr_field_paths:
@@ -96,7 +106,7 @@ def get_blsanalsum_output(field_id=None):
 
         # If no /Cand subdir (old photometry) try upper-dir. If forced here,
         # post-pend "_Old.txt"
-        elif not exists_remote(host_name, nbhr_field_path+'Cand', is_dir=True):
+        else:
             print('{} not found. Trying {}...'.format(nbhr_field_path+'Cand',
                 nbhr_field_path+'blsanalsum.txt'))
             if exists_remote(host_name, nbhr_field_path+'blsanalsum.txt'):
@@ -104,25 +114,35 @@ def get_blsanalsum_output(field_id=None):
                 pull_str = host_name+':'+nbhr_field_path+'blsanalsum.txt '
                 stdout, stderr = run_script('scp '+pull_str+write_str)
             else:
-                print('{} not found. Continue.'.format(\
+                print('{} not found. Continuing.'.format(\
                         nbhr_field_path+'blsanalsum.txt'))
                 continue
 
 
-def parse_blsanalsum_for_BLS_peaks(max_DSP=None):
+def parse_blsanalsum_for_BLS_peaks(max_DSP=None,Ntra_min=3):
     '''
     Take blsanalsum.txt output, parse it into text file of:
     HATID, ROWID, PERIOD, Q, EPOCH, SNR, DSP, NTR, OOTSIG. Save it in
-    /data/HATpipe/blsanalsums/cuts/
+    /data/HATpipe/blsanalsums/cuts/.
+    Impose cuts on minimum number of transits (NTR) and their quality (NTV).
     '''
     assert type(max_DSP) == int, 'call parse_blsanalsum_for_BLS_peaks with max_DSP'
 
     max_DSP = str(max_DSP)
+    # Joel's "NTV" column: 53200. Means 2nd best transit has btwn 50-60% of 
+    # covered pts as best transit. 3rd best has 30-40%. 4th 20-30%. Rest 0-10%.
+    NTV_first_char_atleast = 6
+    NTV_second_char_atleast = 4
     data_path = '../data/HATpipe/blsanalsums/'
     write_path = '../data/HATpipe/blsanalsums/cuts/'
     existing_blsanalsums = os.listdir(data_path)
     blsanalsum_paths = [data_path+eb for eb in existing_blsanalsums if '.txt' \
             in eb]
+
+    print('parse_blsanalsum_for_BLS_peaks w/:\nmax_DSP:{:s}'.format(max_DSP)+\
+        '\nNtra_min:{:d}'.format(Ntra_min)+\
+        '\nNTV_first_char_atleast:{:d}'.format(NTV_first_char_atleast)+\
+        '\nNTV_second_char_atleast:{:d}\n'.format(NTV_second_char_atleast))
 
     for blsanalsum_path in blsanalsum_paths:
         this_file = blsanalsum_path.split('/')[-1]
@@ -132,8 +152,11 @@ def parse_blsanalsum_for_BLS_peaks(max_DSP=None):
         if os.path.exists(out_path):
             print('{:s} exists; continue.'.format(out_path))
         else:
-            gawk_call='gawk \'$16 > {:s} {{print}}\' {:s} '.format(\
-                max_DSP, blsanalsum_path)+\
+            gawk_call='gawk \'($16 > {:s}) && ($25 > {:d}) && '.format(\
+                max_DSP, Ntra_min)+\
+                '(substr($27,1,1) > {:d}) && (substr($27,2,1) > {:d})'.format(\
+                NTV_first_char_atleast, NTV_second_char_atleast)+\
+                ' {{print}}\' {:s} '.format(blsanalsum_path)+\
                 '| gawk \'{printf ("%15s\\t%d\\t%.7f\\t%.5f\\t%.7'+\
                 'f\\t%.5e\\t%.5e\\t%d\\t%.4e\\n", $1, $2, $4, $5, $6,'+\
                 ' $15, $16, $25, $30)}\''+' > {:s}'.format(out_path)
@@ -141,54 +164,120 @@ def parse_blsanalsum_for_BLS_peaks(max_DSP=None):
             run_script(gawk_call)
             print('Parsed {:s}.'.format(out_path))
 
-def sqlitecurve_matching_and_LC_processing(
-        DSP_lim=None,
-        field_id=None,
-        DEBiL_write=False):
+
+def download_parsed_LCs(DSP_lim=None,field_id=None):
     '''
-    Given a specified field (e.g., G199), previous steps have created
-    data/HATpipe/blsanalsums/cuts for that field and neighbors. Now match
-    with sqlitecurve file-IDs. Then create a LC file with format: time,
-    mag, err (space delimited).
-    Pipe these as txt files to /data/LCs_cut.
+    For LCs that met already-imposed cuts on Joel's big legacy BLS run,
+    download their corresponding LCs from the HAT servers for subsequent
+    periodicity analysis.
+    Returns `out`, the astropy table with (ROWID, PERIOD, Q, EPOCH, SN, DSP,
+    NTR, OOTSIG, and has_sqlc) columns.
 
-    Takes ~1 sec per LC (slow in astrobase's hatlc.read*).
+    Args:
+        field_id: HAT-???-1234567
 
-    Optional TODO:
-    * make a few phase-folded LCs (a la astrobase plotting tools) (Not
-        quite necessary yet).
-    * add / use 2MASS IDs in later LC creation
+    (Previously 02_retrive_LCs_given_sqlitecurve_head.py.)
     '''
-    assert type(field_id) == str
+    assert type(field_id)==str and type(DSP_lim)==int
 
-    field_name = 'G'+field_id
-    cut_dpath = '../data/HATpipe/blsanalsums/cuts/'
-    cut_paths = [cut_dpath+p for p in os.listdir(cut_dpath) if
-            'cut_'+str(DSP_lim)+'.txt' in p]
-    col_names = ('HATID', 'ROWID', 'PERIOD', 'Q', 'EPOCH', 'SNR', 'DSP', 'NTR',
-            'OOTSIG')
+    field_name = 'G' + field_id # e.g., 'G081'
+    head_base = '../data/sqlitecurve_headers/'
+    head_path = head_base+field_name+'-sqlitecurves.csv'
+    head_data = pd.read_csv(head_path)
+    # Make the directory to write sqlite.gz light curves
+    write_dir = '../data/LCs/'+field_name
+    if not os.path.exists(write_dir):
+        os.makedirs(write_dir)
 
-    # Get astropy table of blsanalsum for HAT objects with DSP>DSP_lim
-    for ix, cut_path in enumerate(cut_paths):
+    # Cross-reference lcfpaths with /data/HATpipe/blsanalsums/cuts HATIDs
+    names = 'HATID,ROWID,PERIOD,Q,EPOCH,SNR,DSP,NTR,OOTSIG'
+    col_names = tuple([n for n in names.split(',')])
+    # Get neighboring fields, and paths to them on HAT computers.
+    nbhr_fields, nbhr_field_paths = get_neighboring_fields(field_id)
+
+    # For each blsanalsum in this field and neighboring fields, get astropy
+    # table of blsanalsum data for HAT objects with DSP>DSP_lim, Ntra>Ntra_min,
+    # and any other previous cuts.
+    blsanalsum_path = '../data/HATpipe/blsanalsums/cuts/'
+    blsdat_paths = []
+
+    for field in nbhr_fields:
+        thisbls_f = '{:s}_Cand_DSP_cut_{:d}.txt'.format(field_name, DSP_lim)
+        blsdat_paths.append(blsanalsum_path+thisbls_f)
+
+    for ix, blsdat_path in enumerate(blsdat_paths):
         if ix == 0:
-            tab = ascii.read(cut_path, delimiter='\t', names=col_names)
+            tab = ascii.read(blsdat_path, delimiter='\t', names=col_names)
         else:
-            end = ascii.read(cut_path, delimiter='\t', names=col_names)
+            end = ascii.read(blsdat_path, delimiter='\t', names=col_names)
             tab = vstack([tab, end])
 
-    # `tab` df contains all fields. `out` df has those in `field` (e.g., G199).
+    # `tab` df contains all fields. `out` df has those in `field_id` (e.g., G199).
+    # (The specific one being analyzed)
     tab = tab.to_pandas()
     tab = tab.set_index('HATID')
     field_substr = '-'+field_id+'-'
     out = tab[tab.index.str.contains(field_substr)]
 
-    # Cross-match with downloaded sqlitecurves. First, get intersection of out
-    # ['HATID'] and sqlc_names.
-    intersxn = [f[:15] for f in os.listdir('../data/LCs/'+field_name)
-            if f[:15] in out.index]
+    # Cross-match with downloaded sqlitecurves. First, get intersection of
+    # `out` ['HATID'] and sqlc_names.
+    intersxn = [hatid for hatid in out.index if hatid in
+            np.array(head_data.hatid)]
     out['has_sqlc'] = out.index.map(lambda x: True if x in intersxn else False)
 
+    # Download the sqlitecurves for our field that meet our cuts.
+    url_base = 'http://data.hatsurveys.org/archive/'
+
+    lcfpaths = ['hatnet/DR0/{:s}/'.format(field_id)+'/'+hatid+\
+            '-V0-DR0-hatlc.sqlite.gz' for hatid in out.index \
+            if np.all(out.ix[hatid].has_sqlc)]
+    print('Beginning sqlitecurve download for {:s} ({:d} total). Update every 100'.\
+            format(field_name, len(lcfpaths)))
+    k = 0
+    for lcfpath in lcfpaths:
+        # If you already have the LC, don't redownload it. 
+        if os.path.exists(write_dir+'/'+lcfpath.split('/')[-1]):
+            continue
+        else:
+            # Download and write the LC
+            this_url = url_base+lcfpath
+            r = requests.get(this_url)
+            with open(write_dir+'/'+lcfpath.split('/')[-1], 'wb') as data:
+                    data.write(r.content)
+            # Time delay; don't overload the HAT servers
+            if k % 100 == 0:
+                print(k)
+                time.sleep(0.1)
+            k += 1
+
+    print('Done downloading LCs that meet cuts.')
+
+    return out
+
+
+
+
+def periodicity_analysis(out,
+        DSP_lim=None,
+        field_id=None,
+        DEBiL_write=False):
+    '''
+    Given a specified field (e.g., G199), previous steps have created
+    data/HATpipe/blsanalsums/cuts for that field and neighbors (imposing cuts
+    on DSP_lim, Ntra_min, and NTV). They've also downloaded the appropriate
+    LCs.
+    Now rerun the periodicity analysis for these LCs (Box-Least-Squares and
+    Stellingwerf Phase Dispersion Minimization), and make eb_checkplots for
+    subsequent looking-at ("visual inspection").
+
+    Args:
+       DEBiL_write (bool): whether to write a "name and best BLS period" file
+           (in basically all use cases, not necessary).
+    '''
+    assert type(field_id) == str
+
     # File name format: HAT-199-0025234-V0-DR0-hatlc.sqlite.gz
+    field_name = 'G' + field_id # e.g., 'G081'
     LC_read_path = '../data/LCs/'+field_name+'/' # where sqlitecurves already exist
     tail_str = '-V0-DR0-hatlc.sqlite.gz'
     # paths for LCs and EB checkplots
@@ -268,11 +357,11 @@ def sqlitecurve_matching_and_LC_processing(
                 if not os.path.exists(LC_cut_path):
                     copyfile(obj_path, LC_cut_path)
 
-                # Cut: if _all_ nbestperiods > 25 days, likely Cepheid or other
+                # Cut: if _all_ nbestperiods > 30 days, likely Cepheid or other
                 # pulsation periodicity that is not of interest
                 bestperiods = spdmp['nbestperiods']+blsp['nbestperiods']
                 print(bestperiods)
-                period_cut = 25. # days
+                period_cut = 30. # days
                 if np.all(np.array(bestperiods) > period_cut):
                     # Move the eb_checkplot, and the LC to subdirs
                     os.rename(LC_cut_path, LC_periodcut_path)
@@ -300,10 +389,11 @@ if __name__ == '__main__':
     assert len(ao.args) == 2, 'two args allowed: HAT field_id and DSP lim'
 
     # Params needed to identify LCs of interest for periodicity analysis
+    Ntra_min = 3
     field_id = ao.args[0] # keep as string
     DSP_lim = int(ao.args[1])
 
-    # Periodicity analysis
-    get_blsanalsum_output(field_id=field_id)
-    parse_blsanalsum_for_BLS_peaks(max_DSP=DSP_lim)
-    sqlitecurve_matching_and_LC_processing(DSP_lim=DSP_lim, field_id=field_id)
+    scp_blsanalsum_output(field_id=field_id)
+    parse_blsanalsum_for_BLS_peaks(max_DSP=DSP_lim, Ntra_min=Ntra_min)
+    out = download_parsed_LCs(DSP_lim=DSP_lim, field_id=field_id)
+    periodicity_analysis(out, DSP_lim=DSP_lim, field_id=field_id)
